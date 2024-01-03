9f8552b2c48cbddee4d89082451d679cd9b28cd9
Jhenbert Villamucho <jvv205756@gmail.com>
2024-01-03 22:55:19 +0800

    added addWalletListeners fucntion

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import ThemeProvider from "./layout/ThemeProvider";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import RootLayout from "./layout/Root";
 import LandingPage from "./components/LandingPage";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { WalletCtx } from "./context/wallet-ctx";
 import { WindowWithEthereum } from "./types";
 import { ethers } from "ethers";
@@ -56,36 +56,82 @@ function App() {
         console.error(error);
       }
     } else {
-      alert("Please install MetaMask to use this Web3 app.");
+      toast({
+        variant: "destructive",
+        title: "Uh oh! Metamask is not installed",
+        description: "Please install Metamask to interact with this Web3 app",
+      });
     }
   };
 
-  const getCurrentWalletConnected = useCallback(async () => {
+  //Disabled for the meantime until bug is fix
+  // const getCurrentWalletConnected = async () => {
+  //   if ((window as WindowWithEthereum).ethereum) {
+  //     try {
+  //       //Check if there's already request in progess
+  //       if ((window as WindowWithEthereum).ethereum._metamask.isUnlocked()) {
+  //         //get accounts
+  //         // const accounts = await provider.send("eth_requestAccounts", []);
+  //         const accounts = await requestAccount();
+  //         if (!accounts) return;
+
+  //         //get provider
+  //         const provider = new ethers.BrowserProvider(
+  //           (window as WindowWithEthereum).ethereum
+  //         );
+
+  //         //get signer
+  //         setSigner(await provider.getSigner());
+
+  //         //set local contract instance
+  //         setContract(faucetContract(provider));
+
+  //         //set active wallet address
+  //         //if using provider to get accounts, must be accounts[0]
+  //         setWalletAddress(accounts);
+  //       } else {
+  //         toast({
+  //           variant: "destructive",
+  //           title: "Caution!",
+  //           description: "Request in progress. Please wait.",
+  //         });
+  //       }
+  //     } catch (error) {
+  //       console.error((error as Error).message);
+  //     }
+  //   } else {
+  //     toast({
+  //       variant: "destructive",
+  //       title: "Uh oh! Metamask is not installed",
+  //       description: "Please install Metamask to interact with this Web3 app",
+  //     });
+  //   }
+  // };
+
+  const addWalletListener = async () => {
+    //Check if Metamask is installed
     if ((window as WindowWithEthereum).ethereum) {
-      try {
-        //get provider
-        const provider = new ethers.BrowserProvider(
-          (window as WindowWithEthereum).ethereum
-        );
+      (window as WindowWithEthereum).ethereum.on(
+        "accountsChanged",
+        //Update wallet address if user changes wallet address in Metamask
+        (accounts: any) => {
+          setWalletAddress(accounts[0]);
+        }
+      );
+    } else {
+      /**Metamask is not installed
+       * set the wallet address to empty string
+       */
+      setWalletAddress("");
 
-        //get accounts
-        const accounts = await requestAccount();
-        if (!accounts) return;
-
-        //get signer
-        setSigner(await provider.getSigner());
-
-        //set local contract instance
-        setContract(faucetContract(provider));
-
-        //set active wallet address
-        //if using provider to get accounts, must be accounts[0]
-        setWalletAddress(accounts);
-      } catch (error) {
-        console.error((error as Error).message);
-      }
+      //Display error message to user
+      toast({
+        variant: "destructive",
+        title: "Uh oh! Metamask is not installed",
+        description: "Please install Metamask to interact with this Web3 app",
+      });
     }
-  }, []);
+  };
 
   const getTokenHandler = async () => {
     try {
@@ -113,6 +159,7 @@ function App() {
   };
 
   useEffect(() => {
+    //For update soon
     // const getBal = async () => {
     //   if (contract) {
     //     const response = await contract.getBalance();
@@ -120,7 +167,9 @@ function App() {
     //   }
     // };
     // getBal();
-    getCurrentWalletConnected();
+    
+    // getCurrentWalletConnected();
+    addWalletListener();
   }, [walletAddress]);
 
   const value = {
